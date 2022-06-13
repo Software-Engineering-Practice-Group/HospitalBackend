5afe090b4f1321727e0680d9126cdfa0ce12682e
fly0307 <fly0307@sjtu.edu.cn>
2022-06-13 10:55:25 +0800

    注释了返回订单信息部分，其他内容无改动
    注释了返回订单信息部分，其他内容无改动

--- src/main/java/com/example/hospitalbackend/controller/PatientController.java
+++ src/main/java/com/example/hospitalbackend/controller/PatientController.java
@@ -15,26 +15,26 @@ public class PatientController {
     @Autowired
     private UserService userService;
 
-    /*
-     *
-     * @Description: 返回患者订单信息
-     * @param params
-     * @return net.sf.json.JSONObject
-     * @author 赵熙
-     * @date 2022/5/30 9:40
-     * {
-        “name”:”患者姓名” ，
-        “date”:”日期” ,
-        “time” : “挂号具体时间段” ，
-        “deptName” : “科室名称” ,
-        “doctorName” :医生姓名 ，
-        “info”: “病情描述” ，
-        ”process” : “流程”
-        }
-
-     */
-    @RequestMapping("/getUserOrders")
-    List<JSONObject> getAllOrders(@RequestBody Map<String, String> params) {
-        return userService.getAllOrders();
-    }
+//    /*
+//     *
+//     * @Description: 返回患者订单信息
+//     * @param params
+//     * @return net.sf.json.JSONObject
+//     * @author 赵熙
+//     * @date 2022/5/30 9:40
+//     * {
+//        “name”:”患者姓名” ，
+//        “date”:”日期” ,
+//        “time” : “挂号具体时间段” ，
+//        “deptName” : “科室名称” ,
+//        “doctorName” :医生姓名 ，
+//        “info”: “病情描述” ，
+//        ”process” : “流程”
+//        }
+//
+//     */
+//    @RequestMapping("/getUserOrders")
+//    List<JSONObject> getAllOrders(@RequestBody Map<String, String> params) {
+//        return userService.getAllOrders();
+//    }
 }
